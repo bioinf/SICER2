c420571f78c00b5bd32107a6317d4da41931d4ee
Igor V <re.latur@gmail.com>
2016-07-28 14:32:33 +0400

    memory.php version compatibility +

--- stat/memory.php
+++ stat/memory.php
@@ -3,20 +3,20 @@
 # ./memory.php ibezdvornykh
 
 <?php
-$data = []; 
+$data = array(); 
 $x = 0;
 $file = 'memtest.' . time() . '.json';
 while (true) {
 	$user = $_SERVER['argv'][1];
-	$pids = [];
+	$pids = array();
 	exec("ps -u {$user} | grep \"python\" | awk {'print $1'}", $pids);
 	foreach ($pids as $pid) {
-		if (!$data[$pid]) $data[$pid] = [];
-		$mem = []; $sum = 0;
+		if (!$data[$pid]) $data[$pid] = array();
+		$mem = array(); $sum = 0;
 		exec("cat /proc/$pid/status | grep VmSize | awk {'print $2,$3'}", $mem);
 		foreach ($mem as $m) $sum += (int) $mem;
-		$data[$pid][] = [time(), $sum];
-		echo "$pid\tMemory: $m\n";
+		$data[$pid][] = array(time(), $sum);
+		echo "$pid\tMemory: $sum\n";
 	}
 	usleep(100000);
 	$x++;
