e51d2d9142d0a30cd12538f927f1538f51199b45
Igor V <re.latur@gmail.com>
2016-08-26 20:06:12 +0300

    Memory test +

--- stat/memory.php
+++ stat/memory.php
@@ -1,30 +1,33 @@
 #!/usr/bin/php
 # Usage:
 # ./memory.php ibezdvornykh
-
 <?php
 $data = array(); 
 $x = 0;
-$file = 'memtest.' . time() . '.json';
+$file = 'test.' . time() . '.json';
+$user = @$_SERVER['argv'][1];
+if (!@$user) exit;
+echo "# Datafile: $file\n";
 while (true) {
-	$user = $_SERVER['argv'][1];
 	$pids = array();
 	exec("ps -u {$user} | grep \"python\" | awk {'print $1'}", $pids);
 	foreach ($pids as $pid) {
-		if (!$data[$pid]) $data[$pid] = array();
-		$mem = array(); $sum = 0;
+		if (!@$data[$pid]) $data[$pid] = array();
+		$mem = array();
 		exec("cat /proc/$pid/status | grep VmSize | awk {'print $2,$3'}", $mem);
-		foreach ($mem as $m) $sum += (int) $mem;
+		$sum = 0;
+		foreach ($mem as $m) $sum += (int) $m;
+
 		$data[$pid][] = array(time(), $sum);
-		echo "$pid\tMemory: $sum\n";
+		echo "\r$pid\tMemory: $sum [{$x}] ";
 	}
-	usleep(100000);
+	usleep(500000);
 	$x++;
 	if ($x%10 == 0) {
 		$f = fopen($file, 'w+');
 		fputs($f, json_encode($data));
 		fclose($f);
-		echo "> $file; Saved!\n";
 	}
 }
+
 
